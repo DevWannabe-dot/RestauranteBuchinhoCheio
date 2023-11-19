64bdcb887cfa523f27fdc768a0ab28fb0bcb48d9
Pedro <pedro.26.3.h@gmail.com>
2023-11-18 21:28:48 -0300

    v0.2.0.3.4 - desenvolvimento do Menu de Testes

--- TesteRestaurante.java
+++ TesteRestaurante.java
@@ -2,6 +2,8 @@ package com.buchinhocheio.restaurante;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
 
 public class TesteRestaurante {
@@ -31,17 +33,48 @@ public class TesteRestaurante {
                     meuRestaurante.setMesas(mesasCopy);
                 break;
                 case 2:
-                    Iterator<Mesa> it = meuRestaurante.getMesas().iterator();
-
-                    while(it.hasNext()) {
-                        Mesa mesaAtual = it.next();
-                        
-                        System.out.println(String.format("%s | Mesa de número %d", mesaAtual.getData(), mesaAtual.getNumeroMesa()));
-                    }
+                    restaurante_listarMesas(meuRestaurante);
 
                     System.out.print("Digite o número da mesa a detalhar (0 para parar): ");
                     opcao = esc.nextInt();
                     if(opcao > 0) restaurante_detalharMesaN(meuRestaurante, opcao); opcao = OPCAO_NEUTRA;
+                break;
+                case 3:
+                    util_escreverCaracteresln('*', 42);
+                    restaurante_listarMesas(meuRestaurante);
+                    util_escreverCaracteresln('*', 42);
+
+                    System.out.println("Qual o número da mesa onde deseja colocá-l@(s) (0 para parar)? ");
+                    opcao = esc.nextInt();
+                    esc.nextLine();
+
+                    while(opcao > 0 && opcao <= meuRestaurante.getMesas().size()) {                     
+                        if(!(meuRestaurante.getMesas().get(opcao - 1).isReserva())) {
+                            Cliente novoCliente = new Cliente();
+                            List<Cliente> clientes = meuRestaurante.getMesas().get(opcao - 1).getClientes();
+
+                            System.out.print("Insira o nome do seu cliente: ");
+                            novoCliente.setNome(esc.nextLine());
+                            System.out.print("Insira o email do seu cliente: ");
+                            novoCliente.setEmail(esc.nextLine());
+
+                            if(clientes == null) clientes = new ArrayList<Cliente>();
+                            clientes.add(novoCliente);
+
+                            meuRestaurante.getMesas().get(opcao - 1).setClientes(clientes);
+                        } else {
+                            System.out.println("<Mesa indisponível.>");
+                            break;
+                        }
+
+                        System.out.print("Adicionar mais clientes (0 para parar)? ");
+                        boolean continuar = esc.nextInt() != 0;
+                        esc.nextLine();
+                        if(!continuar) break;
+                    }
+
+                    if(opcao > 0 && opcao <= meuRestaurante.getMesas().size()) meuRestaurante.getMesas().get(opcao - 1).reservar(true);
+                    // a partir de agora, a mesa está reservada (ou não)
                 break;
                 default:
                     System.out.println("<Erro na leitura da opção.>");
@@ -52,6 +85,15 @@ public class TesteRestaurante {
         
         esc.close();
     }
+    public static void restaurante_listarMesas(Restaurante r) {
+        Iterator<Mesa> it = r.getMesas().iterator();
+
+        while(it.hasNext()) {
+            Mesa mesaAtual = it.next();
+                       
+            System.out.println(String.format("%s | Mesa de número %d, %s", mesaAtual.getData(), mesaAtual.getNumeroMesa(), (mesaAtual.isReserva() ? "está reservada": "está vazia")));
+        }
+    }
     public static void restaurante_imprimirMenuAdmin(Restaurante r) {
         util_escreverCaracteresln('-', 50);
         System.out.println("Bem-vind@ ao seu restaurante " + r.getNome() +
@@ -59,7 +101,8 @@ public class TesteRestaurante {
         util_escreverCaracteresln('-', 50);
         System.out.print("Selecione sua opção:\n" +
                         "(1) Construir mesa\n" +
-                        "(2) Listar mesas\n" +
+                        "(2) Detalhar mesa\n" +
+                        "(3) Atender cliente\n" +
                         "(0) Log out\n" +
                         ">> ");
     }
@@ -67,24 +110,32 @@ public class TesteRestaurante {
         Iterator<Mesa> it_m = r.getMesas().iterator();
         Mesa mesaEncontrada = null;
 
-        do {
-            mesaEncontrada = it_m.next();
-        } while(it_m.hasNext() && it_m.next().getNumeroMesa() != n);
+        while(it_m.hasNext()) {
+            Mesa mesaAtual = it_m.next();
+
+            mesaEncontrada = mesaAtual.getNumeroMesa() == n ? mesaAtual : null;
+            if(mesaEncontrada != null) break;
+        }
 
         if(mesaEncontrada != null) {
-            util_escreverCaracteresln('*', 30);
-            System.out.println(String.format("%s | Mesa de número %d", mesaEncontrada.getData(), mesaEncontrada.getNumeroMesa()));
-            System.out.println("Clientes: ");
-
-            if(mesaEncontrada.getClientes() != null){
+            util_escreverCaracteresln('*', 42);
+            System.out.println(String.format("%s | Mesa de número %d, %s", mesaEncontrada.getData(), mesaEncontrada.getNumeroMesa(), (mesaEncontrada.isReserva() ? "está reservada": "está vazia")));
+            
+            System.out.println("\tClientes: ");   
+            if(mesaEncontrada.getClientes() != null) {
                 Iterator<Cliente> it_c = mesaEncontrada.getClientes().iterator();
 
                 
                 while(it_c.hasNext()) {
                     Cliente clienteAtual = it_c.next();
 
-                    System.out.print(String.format("%s (%s)", clienteAtual.getNome(), clienteAtual.getEmail()) + (clienteAtual.getComandasDoCliente().size() != 0 ? " fez pedido(s)." : " não fez pedido."));
-                
+                    System.out.println(String.format("\t\t%s (%s)", clienteAtual.getNome(), clienteAtual.getEmail()) + (clienteAtual.getComandasDoCliente().size() != 0 ? " fez pedido(s)." : " não fez pedidos."));
+                }
+            }
+            System.out.println("\tComandas: ");
+            if(mesaEncontrada.getComandasDaMesa() != null) {
+                for(Map.Entry<Mesa, Comanda> entry : mesaEncontrada.getComandasDaMesa().entrySet()) {
+                    System.out.println(entry);
                 }
             }
         }
